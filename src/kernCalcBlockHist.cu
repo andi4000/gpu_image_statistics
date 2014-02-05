ae1e7f63723c1c4cd96f226c94392c51c7e697ce
ariandyy <ariandyy@gmail.com>
2014-02-05 20:39:04 +0100

    analysis in progress. wasnt a bug. sorry.

--- src/kernCalcBlockHist.cu
+++ src/kernCalcBlockHist.cu
@@ -13,7 +13,7 @@ void kernCalcBlockHist(
 	int tid_x = blockIdx.x * strideX + threadIdx.x;
 	int tid_y = cols * blockIdx.y * strideY + cols * threadIdx.y;
 	int tid = tid_y + tid_x;
-	if (blockIdx.x == 31 && blockIdx.y == 0)
+	if (blockIdx.x == 30 && blockIdx.y == 0)
 		cuPrintf("\tblock (%d, %d)\tthread (%d, %d)\ttid (%d + %d)\n", blockIdx.x, blockIdx.y, threadIdx.x, threadIdx.y, tid_y, tid_x);
 		//cuPrintf("(%d, %d)\n", tid_x, tid_y);
 }
