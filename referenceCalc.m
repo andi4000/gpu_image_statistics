72d21ab3b2f018cbe2de58ced400c48f7a162dd1
ariandy <ariandyy@gmail.com>
2014-02-19 00:11:18 +0100

    CHECK THRUST STATISTICS and wikipedia page on Moments mathematics

--- referenceCalc.m
+++ referenceCalc.m
@@ -2,10 +2,11 @@ img = imread("1.png");
 block0 = img(1:32, 1:32);
 block30 = img(481:512, 481:512);
 
-block0_flat = reshape(block0, 1, 32*32);
-block30_flat = reshape(block30, 1, 32*32);
+block0_flat = single(reshape(block0, 1, 32*32));
+block30_flat = single(reshape(block30, 1, 32*32));
 
 block0_flat_sorted = sort(block0_flat);
 block30_flat_sorted = sort(block30_flat);
 
 %mean(mean(block0))
+var(block0_flat) % should return 2033.33
--- src/main.cu
+++ src/main.cu
@@ -5,7 +5,8 @@
  * - cpp and h compiled with g++, and nvcc for cu files
  * - how to separate compilation in cmake and then later link?
  * - implement same function in cpu, then compare
- * - see if Thrust library can help
+ * - see if Thrust library can help 
+ * - https://github.com/thrust/thrust/blob/master/examples/summary_statistics.cu
  * 
  * Specific:
  * - using unsigned int for histogram data yields 1MB of data, which most are useless
